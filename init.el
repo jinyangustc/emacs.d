ac19b943c7168697fa200f3408aecdb39259615b
ljy <mail@jinyangli.org>
2018-01-04 20:01:50 +0800

    learn something from frontmacs

--- init.el
+++ init.el
@@ -2,6 +2,10 @@
 ;;; Commentary:
 ;;; Code:
 
+;;
+;; * System
+;;
+
 ;; system dependent flag
 (defconst *is-a-mac* (eq system-type 'darwin))
 
@@ -46,7 +50,7 @@
 ;;
 (when *is-a-mac*
   (setq mac-command-modifier 'meta)
-  (setq mac-option-modifier 'none)
+  (setq mac-option-modifier 'super)
   (setq-default default-input-method "MacOSX"))
 (setq use-file-dialog nil)
 (setq use-dialog-box nil)
@@ -72,6 +76,9 @@
   :ensure t
   :init (mode-line-bell-mode))
 
+;; fringe width (in pixels)
+(fringe-mode '(8 . 8))
+
 ;;
 ;; * Font and themes
 ;;
@@ -88,19 +95,100 @@
 ;; * Editing
 ;;
 
-(defalias 'yes-or-no-p 'y-or-n-p)
-(setq indent-tabs-mode nil)
+;; don't use tabs to indent
+(setq-default indent-tabs-mode nil)
+
+;; when you have a selection, typing text repalce it all
+(delete-selection-mode t)
+
+;; but maintain correct appearance
+(setq-default tab-width 8)
+
+;; smart tab behavior - indent or complete
+(setq tab-always-indent 'complete)
+
+;; always end files with new line
+(setq require-final-newline t)
+
+;; don't highlight current line
+(global-hl-line-mode -1)
+
+;; enable y/n answers
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; disable lockfiles
+(setq create-lockfiles nil)
+
+;; backup and autosave files go into the tmp directory
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+;; nice scrolling
+(setq scroll-margin 0
+      scroll-conservatively 100000
+      scroll-preserve-screen-position 1)
+
+;; undo visual tree
+(use-package undo-tree
+  :ensure t
+  :diminish undo-tree-mode
+  :config
+  (global-undo-tree-mode 1))
+
+;; setup flycheck to show on the right side of the buffer
+(use-package flycheck
+  :init
+  (setq flycheck-indication-mode 'right-fringe))
+
+;; whitespace
+(use-package whitespace
+  :config
+  (whitespace-mode -1))
+
+;; remove trailing whitespace when saving
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; autosave when switching buffers, windows or frames.
+(defadvice switch-to-buffer (before save-buffer-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice other-window (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-up (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-down (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-left (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-right (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+
+(add-hook 'focus-out-hook
+          (lambda () (when buffer-file-name (save-buffer))))
+
 (winner-mode 1)
-(show-paren-mode 1)
+
+;; show matching paren
+(use-package paren
+  :init
+  (setq show-paren-style 'parenthesis)
+  (show-paren-mode 1))
+
 (remove-hook 'minibuffer-setup-hook 'winner-save-unconditionally)
 (setq backup-by-copying t)
 (setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
 (setq delete-old-versions t)
-(setq create-lockfiles nil)
 (prefer-coding-system 'utf-8)
 (setq delete-by-moving-to-trash t)
+(when (eq system-type 'darwin)
+  (use-package osx-trash
+    :ensure t
+    :config
+    (osx-trash-setup)))
 
+;; revert buffers automatically when underlying files are changed
+;; externally
 (use-package autorevert
   :diminish auto-revert-mode
   :init
@@ -113,8 +201,11 @@
   (setq save-abbrevs nil)
   (setq-default abbrev-mode t))
 
+;; warn when opening file bigger than 100MB
+(setq large-file-warning-threshold 100000000)
+
 ;;
-;; * Company
+;; * Completion
 ;;
 
 (use-package company
@@ -158,6 +249,33 @@ In that case, insert the number."
 			  (self-insert-command 1)))
     (define-key map (kbd "<return>") nil)))
 
+(use-package ivy
+  :ensure t
+  :diminish ivy-mode
+  :bind (("C-s" . counsel-grep-or-swiper)
+	 ("C-r" . swiper))
+  :init
+  (ivy-mode 1)
+  :config
+  (setq ivy-re-builders-alist
+        '((swiper . ivy--regex-plus)
+          (t . ivy--regex-fuzzy))))
+
+(use-package counsel
+  :ensure t
+  :bind ("C-s" . counsel-grep-or-swiper)
+  :bind ("C-x C-r" . counsel-recentf)
+  :bind ("C-c k" . counsel-rg))
+
+(use-package counsel-projectile
+  :ensure t
+  :config
+  (counsel-projectile-mode))
+
+(use-package flx :ensure t)
+
+(use-package smex :ensure t)
+
 ;;
 ;; * Packages
 ;;
@@ -171,10 +289,6 @@ In that case, insert the number."
   :bind ("C-x C-j" . dired-jump)
   :config
   (setq dired-listing-switches "-laGh"))
-
-(use-package diff-hl
-  :ensure t
-  :commands (diff-hl-mode diff-hl-dired-mode))
 
 (use-package anzu
   :ensure t
@@ -191,25 +305,14 @@ In that case, insert the number."
 (use-package ibuffer
   :bind ("C-x C-b" . ibuffer))
 
-(use-package ivy
-  :ensure t
-  :diminish ivy-mode
-  :bind (("C-s" . counsel-grep-or-swiper)
-	 ("C-r" . swiper))
-  :init
-  (ivy-mode 1))
-
-(use-package counsel
-  :ensure t
-  :bind ("C-s" . counsel-grep-or-swiper)
-  :bind ("C-x C-r" . counsel-recentf)
-  :bind ("C-c k" . counsel-rg))
-
 (use-package crux
   :ensure t
-  :bind (("C-k" . crux-smart-kill-line)
+  :bind (("S-RET" . crux-smart-open-line)
+         ("C-k" . crux-smart-kill-line)
 	 ("C-c o" . crux-open-with)
-	 ("C-c I" . crux-find-user-init-file))
+	 ("C-c I" . crux-find-user-init-file)
+         ("C-c d" . crux-duplicate-current-line-or-region)
+         ("C-c M-d" . crux-duplicate-and-comment-current-line-or-region))
   :config
   (global-set-key [remap move-beginning-of-line] #'crux-move-beginning-of-line)
   (global-set-key [remap kill-whole-line] #'crux-kill-whole-line))
@@ -223,6 +326,16 @@ In that case, insert the number."
   :ensure t
   :commands magit-status
   :bind ("C-x g" . magit-status))
+
+(use-package diff-hl
+  :ensure t
+  :commands (diff-hl-mode diff-hl-dired-mode)
+  :config
+  (global-diff-hl-mode t)
+  (diff-hl-flydiff-mode)
+  (diff-hl-dired-mode)
+  (setq diff-hl-draw-borders nil)
+  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh))
 
 (use-package avy
   :ensure t
@@ -290,8 +403,36 @@ In that case, insert the number."
   :ensure t
   :diminish yas-minor-mode
   :config
-  (yas-reload-all)
-  (add-hook 'prog-mode-hook #'yas-minor-mode))
+  (yas-global-mode t))
+
+(use-package projectile
+  :ensure t
+  :diminish projectile-mode
+  :config
+  (projectile-global-mode t)
+  (custom-set-variables
+   '(projectile-switch-project-action (quote projectile-dired)))
+  (setq projectile-completion-system 'ivy))
+
+(use-package windmove
+  :ensure t
+  :config
+  (windmove-default-keybindings 'super))
+
+(use-package key-chord
+  :ensure t
+  :config
+  (key-chord-mode 1)
+  ;; go to word
+  (key-chord-define-global "jj" 'avy-goto-char-timer)
+  ;; go to line
+  (key-chord-define-global "jl" 'avy-goto-line)
+  ;; go back to previous buffer
+  (key-chord-define-global "JJ" 'crux-switch-to-previous-buffer)
+  ;;  undo
+  (key-chord-define-global "uu" 'undo-tree-visualize)
+  ;; shortcut for M-x is nice
+  (key-chord-define-global "xx" 'counsel-M-x))
 
 (use-package which-key
   :ensure t
@@ -300,6 +441,81 @@ In that case, insert the number."
   (which-key-mode)
   (setq which-key-idle-delay 0.5)
   (which-key-setup-side-window-bottom))
+
+(use-package zoom-frm
+  :ensure t)
+
+(use-package emmet-mode
+  :ensure t
+  :init
+  (add-hook 'sgml-mode-hook 'emmet-mode)
+  (add-hook 'web-mode-hook 'emmet-mode)
+  (setq emmet-indentation 2))
+
+(use-package web-mode
+  :ensure t
+  :mode (("\\.html?\\'" . web-mode)
+         ("\\.jsp\\'" . web-mode))
+  :config
+  (custom-set-variables
+   '(web-mode-markup-indent-offset 2)
+   '(web-mode-css-indent-offset 2)
+   '(web-mode-code-indent-offset 2)
+   '(web-mode-enable-auto-quoting nil)
+   '(css-indent-offset 2)))
+
+(use-package js2-mode
+  :ensure t
+  :config
+  (custom-set-variables
+   '(js-indent-level 2)
+   '(js2-basic-offset 2)))
+
+(use-package js2-refactor
+  :ensure t
+  :init
+  (js2r-add-keybindings-with-prefix "C-c C-r")
+  (add-hook 'js2-mode-hook 'js2-refactor-mode-hook))
+
+(use-package js-doc
+  :ensure t
+  :bind (:map js2-refactor-mode-map
+              ("C-c C-r i d" . js-doc-insert-function-doc)
+              ("@" . js-doc-insert-tag)))
+
+(use-package tide
+  :ensure t
+  :init
+  (defun setup-tide-mode ()
+    (interactive)
+    (tide-setup)
+    (flycheck-mode 1)
+    (setq flycheck-check-syntax-automatically '(save mode-enable))
+    (eldoc-mode 1)
+    (tide-hl-identifier-mode 1)
+    (company-mode 1))
+  (add-hook 'typescript-mode-hook #'setup-tide-mode)
+  (add-hook 'before-save-hook 'tide-format-before-save)
+  (setq tide-format-options
+        '(:indentSize 2 :tabSize 2)))
+
+(use-package compile
+  :init
+  (add-to-list 'compilation-error-regexp-alist 'node)
+  (add-to-list 'compilation-error-regexp-alist-alist
+               '(node "^[[:blank:]]*at \\(.*(\\|\\)\\(.+?\\):\\([[:digit:]]+\\):\\([[:digit:]]+\\)" 2 3 4)))
+
+(use-package rainbow-mode
+  :ensure t
+  :init
+  (rainbow-mode 1))
+
+(use-package css-mode
+  :mode (("\\.css\\'" . css-mode)
+         ("\\.scss\\'" . css-mode)
+         ("\\.sass\\'" . css-mode))
+  :init
+  (setq css-indent-offset 2))
 
 (provide 'init)
 ;;; init.el ends here
