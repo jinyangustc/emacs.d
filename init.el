2d6b5d91d77cd80de47827ec0820ac152a55bd83
ljy <mail@jinyangli.org>
2018-01-04 22:44:52 +0800

    disable autosave

--- init.el
+++ init.el
@@ -151,21 +151,20 @@
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 ;; autosave when switching buffers, windows or frames.
-(defadvice switch-to-buffer (before save-buffer-now activate)
-  (when buffer-file-name (save-buffer)))
-(defadvice other-window (before other-window-now activate)
-  (when buffer-file-name (save-buffer)))
-(defadvice windmove-up (before other-window-now activate)
-  (when buffer-file-name (save-buffer)))
-(defadvice windmove-down (before other-window-now activate)
-  (when buffer-file-name (save-buffer)))
-(defadvice windmove-left (before other-window-now activate)
-  (when buffer-file-name (save-buffer)))
-(defadvice windmove-right (before other-window-now activate)
-  (when buffer-file-name (save-buffer)))
-
-(add-hook 'focus-out-hook
-          (lambda () (when buffer-file-name (save-buffer))))
+;; (defadvice switch-to-buffer (before save-buffer-now activate)
+;;   (when buffer-file-name (save-buffer)))
+;; (defadvice other-window (before other-window-now activate)
+;;   (when buffer-file-name (save-buffer)))
+;; (defadvice windmove-up (before other-window-now activate)
+;;   (when buffer-file-name (save-buffer)))
+;; (defadvice windmove-down (before other-window-now activate)
+;;   (when buffer-file-name (save-buffer)))
+;; (defadvice windmove-left (before other-window-now activate)
+;;   (when buffer-file-name (save-buffer)))
+;; (defadvice windmove-right (before other-window-now activate)
+;;   (when buffer-file-name (save-buffer)))
+;; (add-hook 'focus-out-hook
+;;           (lambda () (when buffer-file-name (save-buffer))))
 
 (winner-mode 1)
 
