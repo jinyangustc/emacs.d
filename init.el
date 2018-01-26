f4813a11ef7cb700eb478cfd4217867fceffb30a
ljy <mail@jinyangli.org>
2018-01-26 19:52:04 +0800

    OCaml support

--- init.el
+++ init.el
@@ -740,6 +740,42 @@ In that case, insert the number."
   :bind (:map flyspell-mode-map
               ("<f8>" . flyspell-correct-next-word-generic)))
 
+(use-package tuareg
+  :ensure t
+  :defer t
+  :mode (("\\.ml[ily]?$" . tuareg-mode)
+         ("\\.topml$" . tuareg-mode)))
+
+(use-package merlin
+  :ensure t
+  :defer t
+  :init
+  (add-hook 'tuareg-mode-hook 'merlin-mode)
+  (add-hook 'merlin-mode-hook #'company-mode)
+  (setq merlin-error-after-save nil))
+
+(use-package ocp-indent
+  :ensure t
+  :defer t
+  :init
+  (add-hook 'tuareg-mode-hook 'ocp-indent-caml-mode-setup))
+
+(use-package utop
+  :ensure t
+  :defer t
+  :init
+  (add-hook 'tuareg-mode-hook 'utop-minor-mode)
+  :config
+  (if (executable-find "opam")
+      (setq utop-command "opam config exec -- utop -emacs")
+    (message "Cannot find opam executable.")))
+
+(use-package flycheck-ocaml
+  :ensure t
+  :defer t
+  :init
+  (flycheck-ocaml-setup))
+
 
 ;; (use-package cnfonts
 ;;   :ensure t
