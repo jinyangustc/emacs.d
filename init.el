bab47050f0e80b51c6f70a651c4f86650fac9948
ljy <mail@jinyangli.org>
2018-01-26 20:07:34 +0800

    Haskell support

--- init.el
+++ init.el
@@ -776,6 +776,62 @@ In that case, insert the number."
   :init
   (flycheck-ocaml-setup))
 
+(use-package company-cabal
+  :ensure t
+  :defer t
+  :init
+  (add-to-list 'company-backends 'company-cabal))
+
+(use-package company-ghc
+  :ensure t
+  :defer t)
+
+(use-package intero
+  :ensure t
+  :defer t)
+
+(use-package flycheck-haskell
+  :ensure t
+  :defer t
+  :init
+  (add-hook 'flycheck-mode-hook 'flycheck-haskell-configure))
+
+(use-package haskell-mode
+  :ensure t
+  :defer t
+  :init
+  (setq haskell-interactive-popup-errors nil
+        haskell-process-suggest-remove-import-lines t
+        haskell-process-auto-import-loaded-modules t
+        haskell-stylish-on-save nil)
+  :config
+  (with-eval-after-load 'align
+    (add-to-list 'align-rules-list
+                 '(haskell-types
+                   (regexp . "\\(\\s-+\\)\\(::\\|∷\\)\\s-+")
+                   (modes . haskell-modes)))
+    (add-to-list 'align-rules-list
+                 '(haskell-assignment
+                   (regexp . "\\(\\s-+\\)=\\s-+")
+                   (modes . haskell-modes)))
+    (add-to-list 'align-rules-list
+                 '(haskell-arrows
+                   (regexp . "\\(\\s-+\\)\\(->\\|→\\)\\s-+")
+                   (modes . haskell-modes)))
+    (add-to-list 'align-rules-list
+                 '(haskell-left-arrows
+                   (regexp . "\\(\\s-+\\)\\(<-\\|←\\)\\s-+")
+                   (modes . haskell-modes)))))
+
+(use-package hindent
+  :ensure t
+  :defer t
+  :init
+  (add-hook 'haskell-mode-hook #'hindent-mode))
+
+(use-package hlint-refactor
+  :ensure t
+  :defer t)
 
 ;; (use-package cnfonts
 ;;   :ensure t
